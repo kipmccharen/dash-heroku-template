83e2a33da3f460bd4827041ee95feca0962b9744
kipmccharen <kip.mccharen@gmail.com>
2020-11-19 13:38:07 -0500

    dash boostrap components import

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dash import Dash
 #from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from matplotlib import pyplot as plt
 from matplotlib import colors as pltcolors
